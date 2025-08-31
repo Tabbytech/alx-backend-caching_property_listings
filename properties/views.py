2c92d44e0ea30748200e916a25068cc6e438b418
Tabbytech <tabbyccherop@gmail.com>
2025-08-31 11:21:38 +0300

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,25 @@
 from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 from .utils import get_all_properties, get_redis_cache_metrics
+from .models import Property
 
+
+# View with low-level caching (queryset)
 def property_list(request):
     properties = get_all_properties()
     return JsonResponse(properties, safe=False)
 
+
+# View with response caching (15 minutes)
+@cache_page(60 * 15)
+def property_list_cached(request):
+    properties = Property.objects.all().values(
+        "id", "title", "description", "price", "location", "created_at"
+    )
+    return JsonResponse(list(properties), safe=False)
+
+
+# Redis metrics view
 def cache_metrics(request):
     metrics = get_redis_cache_metrics()
     return JsonResponse(metrics)
